99598ca0a8faff65a0931623a578e5986a5b651f
Dmitry Vishniakov <112121dfd@gmail.com>
2023-04-30 15:51:45 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int costTicket = 13676;
+        int bonusBalance = costTicket / 20;
+
+        System.out.println("Начислено бонусов " + bonusBalance);
+    }
+}
